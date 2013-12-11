e36ecf4896b2e5d6b7244cac2628f4ec6118063c
Troy Whiteley <troy@somanyscientists.com>
2013-12-10 20:10:30 -0800

    Added method to translate months and days.
    Fixes #2

--- test/dateformat.test.js
+++ test/dateformat.test.js
@@ -1,71 +1,80 @@
 
-suite('DateFormat', function() {
+suite('dateFormat', function() {
+  test('day', function() {
+    var date = new Date(2013, 4, 8);
 
-  suite('time format', function() {
-    test('day', function() {
-      var date = new Date(2013, 4, 8);
+    assert.equal(dateFormat('%d %j %D %l', date), '08 8 Wed Wednesday');
 
-      assert.equal(DateFormat('%d %j %D %l', date), '08 8 Wed Wednesday');
+    var date2 = new Date(2013, 4, 10);
 
-      var date2 = new Date(2013, 4, 10);
+    assert.equal(dateFormat('%d %j %D %l', date2), '10 10 Fri Friday');
+  });
 
-      assert.equal(DateFormat('%d %j %D %l', date2), '10 10 Fri Friday');
-    });
+  test('month', function() {
+    var date = new Date(2013, 3, 15);
 
-    test('month', function() {
-      var date = new Date(2013, 3, 15);
+    assert.equal(dateFormat('%n %F %m %M', date), '3 April 04 Apr');
 
-      assert.equal(DateFormat('%n %F %m %M', date), '3 April 04 Apr');
+    var date2 = new Date(2013, 11, 15);
 
-      var date2 = new Date(2013, 11, 15);
+    assert.equal(dateFormat('%n %F %m %M', date2), '11 December 12 Dec');
+  });
 
-      assert.equal(DateFormat('%n %F %m %M', date2), '11 December 12 Dec');
-    });
+  test('week', function() {
+    var date = new Date(2013, 4, 15);
 
-    test('week', function() {
-      var date = new Date(2013, 4, 15);
+    assert.equal(dateFormat('%w', date), '3');
+  });
 
-      assert.equal(DateFormat('%w', date), '3');
-    });
+  test('year', function() {
+    var date = new Date(2013, 4, 15);
 
-    test('year', function() {
-      var date = new Date(2013, 4, 15);
+    assert.equal(dateFormat('%Y %y', date), '2013 13');
+  });
 
-      assert.equal(DateFormat('%Y %y', date), '2013 13');
-    });
+  test('time', function() {
+    var date = new Date(2013, 4, 15, 8, 30, 30, 50);
 
-    test('time', function() {
-      var date = new Date(2013, 4, 15, 8, 30, 30, 50);
+    assert.equal(dateFormat('%g %G %h %h %i %s %u %a %A %e', date), '8 8 08 08 30 30 50 am AM ' + date.getTimezoneOffset());
 
-      assert.equal(DateFormat('%g %G %h %h %i %s %u %a %A %e', date), '8 8 08 08 30 30 50 am AM ' + date.getTimezoneOffset());
+    date = new Date(2013, 4, 15, 18, 5, 5, 5);
 
-      date = new Date(2013, 4, 15, 18, 5, 5, 5);
+    assert.equal(dateFormat('%g %G %h %H %i %s %u %a %A %e', date), '6 18 06 18 05 05 5 pm PM ' + date.getTimezoneOffset());
+  });
 
-      assert.equal(DateFormat('%g %G %h %H %i %s %u %a %A %e', date), '6 18 06 18 05 05 5 pm PM ' + date.getTimezoneOffset());
-    });
+  test('st,nd,rt,th', function() {
+    var date = new Date(2013, 4, 1);
 
-    test('st,nd,rt,th', function() {
-      var date = new Date(2013, 4, 1);
+    assert.equal(dateFormat('%S', date), 'st');
 
-      assert.equal(DateFormat('%S', date), 'st');
+    date = new Date(2013, 4, 2);
 
-      date = new Date(2013, 4, 2);
+    assert.equal(dateFormat('%S', date), 'nd');
 
-      assert.equal(DateFormat('%S', date), 'nd');
+    date = new Date(2013, 4, 3);
 
-      date = new Date(2013, 4, 3);
+    assert.equal(dateFormat('%S', date), 'rd');
 
-      assert.equal(DateFormat('%S', date), 'rd');
+    date = new Date(2013, 4, 15);
 
-      date = new Date(2013, 4, 15);
+    assert.equal(dateFormat('%S', date), 'th');
+  });
 
-      assert.equal(DateFormat('%S', date), 'th');
-    });
+  test('midnight should be 12', function() {
+    var date = new Date(2013, 4, 1);
 
-    test('midnight should be 12', function() {
-      var date = new Date(2013, 4, 1);
+    assert.equal(12, dateFormat('%g', date));
+  });
 
-      assert.equal(12, DateFormat('%g', date));
-    });
+  test('translated month and days', function() {
+    var date = new Date(2013, 0, 6);
+
+    var months = ['Enero'];
+    var days = ['Lunes'];
+
+    dateFormat.translate(months, days);
+
+    assert.equal(dateFormat('%F', date), 'Enero');
+    assert.equal(dateFormat('%l', date), 'Lunes');
   });
 });
